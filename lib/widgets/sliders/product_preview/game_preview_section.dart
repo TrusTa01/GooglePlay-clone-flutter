f8f07420271ff229afc9b1d544e995e77efd1914
Yura Borovikov <danylnet65@gmail.com>
2026-01-29 23:59:21 +1000

    make ProductPreviewCard universal

---
+++ lib/widgets/sliders/product_preview/game_preview_section.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/models.dart';
+import '../../widgets.dart';
+
+class GamePreviewSection extends StatelessWidget {
+  final List<Game> game;
+  final bool nestedInScrollView;
+
+  const GamePreviewSection({
+    super.key,
+    required this.game,
+    this.nestedInScrollView = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (game.isEmpty) {
+      debugPrint('Ошибка: products.isEmpty (game preview section)');
+      return const SizedBox.shrink();
+    }
+
+    return ListView.separated(
+      shrinkWrap: nestedInScrollView,
+      physics: nestedInScrollView ? const NeverScrollableScrollPhysics() : null,
+      separatorBuilder: (context, index) => const SizedBox(height: 35),
+      itemCount: game.length,
+      itemBuilder: (context, index) {
+        return ProductPreviewCard(product: game[index]);
+      },
+    );
+  }
+}
---
+++ lib/widgets/sliders/product_preview/product_preview_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '/widgets/widgets.dart';
+import '/models/models.dart';
+
+class ProductPreviewCard extends StatelessWidget {
+  final Product product;
+  final bool showActionRow;
+
+  const ProductPreviewCard({
+    super.key,
+    required this.product,
+    this.showActionRow = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 180,
+          child: ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            itemCount: product.screenshots.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: ProductScreenshotImage(
+                  imageUrl: product.screenshots[index],
+                  productId: product.id,
+                  index: index,
+                ),
+              );
+            },
+          ),
+        ),
+        if (showActionRow) ...[
+          const SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 22),
+            child: ActionRow(product: product, showButton: true),
+          ),
+        ],
+      ],
+    );
+  }
+}
