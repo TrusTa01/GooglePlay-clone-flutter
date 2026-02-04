72ee3bce31c846ed3107ac19a5ceeb8e581c74ec
Yura Borovikov <danylnet65@gmail.com>
2026-02-04 20:52:19 +1000

    Fixed overflow in category section by correcting aspectRatio

--- lib/widgets/category_details_section.dart
+++ lib/widgets/category_details_section.dart
@@ -13,8 +13,6 @@ class CategoryDetailsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final bool isBook = products.first is Book;
     final double minItemWidth = isBook ? 100 : 125;
-    // Высота текстовой части: SizedBox(6) + Title 2 строки (~36) + SizedBox(4) + Rating(~18) + запас
-    const double textAreaHeight = 90;
 
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
@@ -25,22 +23,9 @@ class CategoryDetailsSection extends StatelessWidget {
 
         // Фиксированные отступы
         final double horizontalPadding = 16;
-        final double availableWidth =
-            constraints.maxWidth - (horizontalPadding * 2);
-        final double totalSpacing = (crossAxisCount - 1) * 10;
-
-        // Вычисляем реальную ширину элемента
-        final double itemWidth =
-            (availableWidth - totalSpacing) / crossAxisCount;
-
-        // Динамически вычисляем высоту: иконка + текстовая часть
-        // Книги: соотношение 2:3 (height = width * 1.5)
-        // Приложения: соотношение 1:1
-        final double iconHeight = isBook ? itemWidth * 1.5 : itemWidth;
-        final double itemHeight = iconHeight + textAreaHeight;
 
         // Вычисляем aspectRatio
-        final double aspectRatio = itemWidth / itemHeight;
+        final double aspectRatio = isBook ? 0.5 : 0.6;
 
         return Center(
           child: ConstrainedBox(
@@ -52,7 +37,7 @@ class CategoryDetailsSection extends StatelessWidget {
                 horizontal: horizontalPadding,
                 vertical: 16,
               ),
-            
+
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: crossAxisCount,
                 childAspectRatio: aspectRatio,
@@ -64,7 +49,7 @@ class CategoryDetailsSection extends StatelessWidget {
                 final product = products[index];
                 final showPrice = product.isPaid && product.price != null;
                 final formatter = ProductDataFormatter(product);
-            
+
                 return ProductCardContent(
                   product: product,
                   showPrice: showPrice,
