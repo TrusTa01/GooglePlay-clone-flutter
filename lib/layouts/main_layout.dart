ed8ab8d2cea4d0355a20af06e8beed3fdb5ab30a
Yura Borovikov <danylnet65@gmail.com>
2026-02-17 19:01:44 +1000

    Use ValueNotifier for tab index in MainLayout - Replace setState with ValueNotifier so only Scaffold/IndexedStack/nav bar rebuild on tab change - Add dispose for the notifier and simplify tab handlers to one-liners

--- lib/layouts/main_layout.dart
+++ lib/layouts/main_layout.dart
@@ -13,7 +13,7 @@ class MainLayout extends StatefulWidget {
 }
 
 class _MainLayoutState extends State<MainLayout> {
-  int _currentPageIndex = 0;
+  final ValueNotifier<int> _currentPageIndex = ValueNotifier<int>(0);
 
   final List<GlobalKey<NavigatorState>> _navigatorKeys = [
     GlobalKey<NavigatorState>(), // Games
@@ -21,7 +21,13 @@ class _MainLayoutState extends State<MainLayout> {
     GlobalKey<NavigatorState>(), // Search
     GlobalKey<NavigatorState>(), // Books
   ];
-  
+
+  @override
+  void dispose() {
+    _currentPageIndex.dispose();
+    super.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -33,42 +39,37 @@ class _MainLayoutState extends State<MainLayout> {
   }
 
   // Обработчик выбора вкладки
-  void _handleTabSelection(int index) {
-    if (index == _currentPageIndex) {
-      _popToRoot(index);
-    } else {
-      _switchTab(index);
-    }
-  }
+  void _handleTabSelection(int index) =>
+      index == _currentPageIndex.value ? _popToRoot(index) : _switchTab(index);
 
   // Переключение на другую вкладку
-  void _popToRoot(int index) {
-    _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
-  }
+  void _popToRoot(int index) =>
+      _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
 
   // Переключение вкладки
-  void _switchTab(int index) {
-    setState(() {
-      _currentPageIndex = index;
-    });
-  }
+  void _switchTab(int index) => _currentPageIndex.value = index;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentPageIndex,
-        children: [
-          _navigatorKeys.createNavigator(0, const GamesScreen()),
-          _navigatorKeys.createNavigator(1, const AppsScreen()),
-          _navigatorKeys.createNavigator(2, const SearchScreen()),
-          _navigatorKeys.createNavigator(3, const BooksScreen()),
-        ],
-      ),
-      bottomNavigationBar: CustomNavigationBar(
-        currentPageIndex: _currentPageIndex,
-        onDestinationSelected: _handleTabSelection,
-      ),
+    return ValueListenableBuilder<int>(
+      valueListenable: _currentPageIndex,
+      builder: (context, currentIndex, _) {
+        return Scaffold(
+          body: IndexedStack(
+            index: currentIndex,
+            children: [
+              _navigatorKeys.createNavigator(0, const GamesScreen()),
+              _navigatorKeys.createNavigator(1, const AppsScreen()),
+              _navigatorKeys.createNavigator(2, const SearchScreen()),
+              _navigatorKeys.createNavigator(3, const BooksScreen()),
+            ],
+          ),
+          bottomNavigationBar: CustomNavigationBar(
+            currentPageIndex: currentIndex,
+            onDestinationSelected: _handleTabSelection,
+          ),
+        );
+      },
     );
   }
 }
